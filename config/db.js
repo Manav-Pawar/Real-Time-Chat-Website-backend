e76a05bb5f2273902a12650d7899f2317b220e5d
Manav-Pawar <pawar.manav2304@gmail.com>
2025-01-24 15:15:09 +0530

    change db.js add process.env.db in file

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/chatapp', {
+    const mongoURI = process.env.MONGODB_URI
+    await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
